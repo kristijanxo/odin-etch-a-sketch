7e280540257d23c31dce1e0eedb311a27e447a90
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-11-07 21:31:12 +0100

    Refactor script to utilize functions and implement custom gridsizes

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for (let i = 0; i < 256; i++) {
-  const gridElement = document.createElement("div");
-  gridElement.classList.add("grid-element");
-  gridContainer.appendChild(gridElement);
+function createGrid(gridSize = 16) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridElement = document.createElement("div");
+    const cellSize = 100 / gridSize;
+    gridElement.classList.add("grid-element");
+    gridElement.style.flex = `0 0 ${cellSize}%`;
+    gridContainer.appendChild(gridElement);
+  }
 }
 
-const allGridElements = document.querySelectorAll(".grid-element");
-
-allGridElements.forEach((element) => {
-  element.addEventListener("mouseenter", () => {
-    // element.style.backgroundColor = "red";
-    element.classList.add("colored-grid-element");
-  });
-});
-
-const resetButton = document.querySelector("#reset-button");
-resetButton.addEventListener("click", () => {
+function addColorationListeners() {
+  const allGridElements = document.querySelectorAll(".grid-element");
   allGridElements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      // element.style.backgroundColor = "red";
+      element.classList.add("colored-grid-element");
+    });
+  });
+}
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  document.querySelectorAll(".grid-element").forEach((element) => {
     element.classList.remove("colored-grid-element");
   });
 });
+
+const changeGridButton = document.querySelector("#change-grid-button");
+changeGridButton.addEventListener("click", () => {
+  const newGridSize = prompt(
+    "Enter new grid size (applies to H×W, e.g., 16)",
+    16,
+  );
+
+  gridContainer.replaceChildren();
+  createGrid(newGridSize);
+  addColorationListeners();
+});
+
+createGrid();
+addColorationListeners();
