f27a60ce8a75ec8ee26f9a72336c114e6163eca7
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-11-07 17:24:27 +0100

    Implement coloration upon mouseenter and add a reset button

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ for (let i = 0; i < 256; i++) {
   gridElement.classList.add("grid-element");
   gridContainer.appendChild(gridElement);
 }
+
+const allGridElements = document.querySelectorAll(".grid-element");
+
+allGridElements.forEach((element) => {
+  element.addEventListener("mouseenter", () => {
+    // element.style.backgroundColor = "red";
+    element.classList.add("colored-grid-element");
+  });
+});
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => {
+  allGridElements.forEach((element) => {
+    element.classList.remove("colored-grid-element");
+  });
+});
